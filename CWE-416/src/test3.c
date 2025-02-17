fac1d8a1794c885b354c1fc196d36ecc0ca21cee
codehelixtest <rogern@nealit.com>
2025-02-16 22:32:25 -0800

    Fix vulnerability in test3.c

--- CWE-416/src/test3.c
+++ CWE-416/src/test3.c
@@ -20,6 +20,24 @@ void dangerous_func ( int* ptr , int a, int b) {
 
 int main () {
      /* Unsafe function call */
-     dangerous_func(malloc(sizeof ( int)),0,0) ;
+void dangerous_func ( int* ptr , int a, int b) {
+     int val = 0;
+     if (!ptr) return;
+     if(a) {
+         *ptr += 2;
+     } else {
+         val = *ptr;
+         free(ptr);
+         return; // Exit after freeing to prevent further use
+     }
+     if(b) {
+         val += 5;
+     } else {
+         // Do not dereference ptr here as it has been freed
+         return; // Exit to prevent use after free
+     }
+     if(a) free(ptr);
+     printf ("val = %i\n", val);
+ }
      return 0;
 }
