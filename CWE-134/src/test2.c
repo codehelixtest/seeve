2e4c59f0dc5354b79ac43b9b2c66231316f73206
codehelixtest <rogern@nealit.com>
2025-02-16 22:33:19 -0800

    Fix vulnerability in test2.c

--- CWE-134/src/test2.c
+++ CWE-134/src/test2.c
@@ -6,9 +6,9 @@ int main (int argc, char **argv)
 {
 	char buf [100];
 	int x = 1 ; 
-	snprintf(buf,sizeof buf, argv [1]);
+snprintf(buf, sizeof buf, "%s", argv[1]); if (strlen(argv[1]) >= sizeof(buf)) { fprintf(stderr, "Input too long"); exit(EXIT_FAILURE); }
 	buf[sizeof buf -1 ] = 0;
-snprintf(buf, sizeof(buf), "%s", argv[1]);
+	printf("Buffer size is: (%d) \n Data input: %s \n",strlen(buf),buf);
 	printf("X equals: %d in hex: %#x\n Memory address for x: (%p) \n",x,x,&x);
 	return 0 ;
 }
