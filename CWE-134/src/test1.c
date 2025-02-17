c6c2d3121d610af36ab0b04630fd1427ab5a3ac9
codehelixtest <rogern@nealit.com>
2025-02-16 22:33:08 -0800

    Fix vulnerability in test1.c

--- CWE-134/src/test1.c
+++ CWE-134/src/test1.c
@@ -2,12 +2,12 @@
 #include <string.h>
 
 void printWrapper(char *string) {
-    printf(string);
+void printWrapper(const char *string) { printf("%s", string); }
 }
 
 int main(int argc, char **argv) {
     char buf[5012];
-if (argc > 1 && argv[1] != NULL) { memcpy(buf, argv[1], 5012); printWrapper(argv[1]); } else { printf("No input provided\n"); }
+    memcpy(buf, argv[1], 5012);
     printWrapper(argv[1]);
     return 0;
 }
