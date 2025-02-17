3d9d48716629e59ce4f3c01a2cca084e1fe4615e
codehelixtest <rogern@nealit.com>
2025-02-16 22:32:48 -0800

    Fix vulnerability in test1.c

--- CWE-415/src/test1.c
+++ CWE-415/src/test1.c
@@ -1,16 +1,15 @@
 #include <stdlib.h>
 #define SIZE 16
 int main(){
-#include <stdlib.h>
-#define SIZE 16
-int main(){
-    char* ptr = (char*)malloc(SIZE);
-    if (ptr) {
-        // Use ptr for intended operations
-        free(ptr);
+    #include <stdlib.h>
+    #define SIZE 16
+    int main(){
+        char* ptr = (char*)malloc (SIZE);
+        if (ptr) {
+            free(ptr);
+        }
+        return 0;
     }
-    return 0;
-}
     if (1) {
         free(ptr);
     }
