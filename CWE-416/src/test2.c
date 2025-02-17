bd872a7fd671058cf6cfcc38e8cdaf202ab9bc69
codehelixtest <rogern@nealit.com>
2025-02-16 22:33:21 -0800

    Fix vulnerability in test2.c

--- CWE-416/src/test2.c
+++ CWE-416/src/test2.c
@@ -6,17 +6,12 @@
 int main (){
     int abrt=0;
     int err=1;
-char* ptr = (char*) malloc(SIZE*sizeof(char));
-if (ptr == NULL) {
-    fprintf(stderr, "Memory allocation failed\n");
-    return 1;
-}
-strcpy(ptr,"This string is in the heap");
+    char* ptr = (char*) malloc(SIZE*sizeof(char));
     strcpy(ptr,"This string is in the heap");
     if (err) {
         abrt = 1;
         free(ptr);
-        char* ptr2 = (char*) malloc(2*sizeof(char));
+char* ptr = (char*) malloc(SIZE*sizeof(char)); if (ptr == NULL) { fprintf(stderr, "Memory allocation failed\n"); exit(EXIT_FAILURE); }
     }
     if (abrt) {
         printf("operation aborted before commit. Pointer value is ptr: %s",ptr);
