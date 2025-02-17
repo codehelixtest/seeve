7a8bfadd615665544be4b93376a52833f2af1a27
codehelixtest <rogern@nealit.com>
2025-02-16 22:33:11 -0800

    Fix vulnerability in test1.c

--- CWE-665/src/test1.c
+++ CWE-665/src/test1.c
@@ -2,12 +2,7 @@
 #include <string.h>
 int main(){
     char str[20];
-int main(){
-    char str[20] = "";
-    strncat(str, "hello world", sizeof(str) - strlen(str) - 1);
-    printf("%s", str);
-    return 0;
-}
+char str[20] = ""; strcat(str, "hello world");
     printf("%s", str);
     return 0;
 }
