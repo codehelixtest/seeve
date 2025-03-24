b0f75196e3a7bd48b8ba0e349a01472833aef32a
codehelixtest <rogern@nealit.com>
2025-03-24 15:43:45 -0700

    Fix vulnerability in test1.c

--- CWE-416/src/test1.c
+++ CWE-416/src/test1.c
@@ -10,12 +10,12 @@
         char *buf2R1;
         char *buf2R2;
         char *buf3R2;
-        buf1R1 = (char *) malloc(BUFSIZER1);
+if (buf1R1 == NULL) { perror("malloc failed"); exit(EXIT_FAILURE); } buf1R1 = (char *) malloc(BUFSIZER1);
         buf2R1 = (char *) malloc(BUFSIZER1);
         free(buf2R1);
         buf2R2 = (char *) malloc(BUFSIZER2);
         buf3R2 = (char *) malloc(BUFSIZER2);
-strncpy(buf2R1, argv[1], BUFSIZER1-1); // Move this line before free(buf2R1);
+        strncpy(buf2R1, argv[1], BUFSIZER1-1);
         free(buf1R1);
         free(buf2R2);
         free(buf3R2);
