29b7c0cf1978c3a2cbb4c6152d5d3f534952d8f4
codehelixtest <rogern@nealit.com>
2025-03-24 15:43:53 -0700

    Fix vulnerability in test2.c

--- CWE-416/src/test2.c
+++ CWE-416/src/test2.c
@@ -6,17 +6,14 @@
 int main (){
     int abrt=0;
     int err=1;
-if (err) {
-    abrt = 1;
-    free(ptr);
-    ptr = NULL; // Set ptr to NULL after freeing
-    char* ptr2 = (char*) malloc(2*sizeof(char));
-}
+    char* ptr = (char*) malloc(SIZE*sizeof(char));
     strcpy(ptr,"This string is in the heap");
     if (err) {
         abrt = 1;
         free(ptr);
-        char* ptr2 = (char*) malloc(2*sizeof(char));
+if (abrt) {
+    printf("operation aborted before commit. Pointer value is ptr: %s", ptr ? ptr : "NULL");
+}
     }
     if (abrt) {
         printf("operation aborted before commit. Pointer value is ptr: %s",ptr);
