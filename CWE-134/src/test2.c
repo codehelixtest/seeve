aef96e913eb70141ebbfe99d4a42c93a6a0faa16
codehelixtest <rogern@nealit.com>
2025-03-24 15:43:27 -0700

    Fix vulnerability in test2.c

--- CWE-134/src/test2.c
+++ CWE-134/src/test2.c
@@ -6,7 +6,7 @@ int main (int argc, char **argv)
 {
 	char buf [100];
 	int x = 1 ; 
-	snprintf(buf,sizeof buf, argv [1]);
+if (argc > 1) { snprintf(buf, sizeof buf, argv[1]); } else { snprintf(buf, sizeof buf, ""); }
 	buf[sizeof buf -1 ] = 0;
 	printf("Buffer size is: (%d) \n Data input: %s \n",strlen(buf),buf);
 	printf("X equals: %d in hex: %#x\n Memory address for x: (%p) \n",x,x,&x);
