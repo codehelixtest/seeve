91c092c63f01e60b1da4e91f47d905d46ae802bc
codehelixtest <rogern@nealit.com>
2025-04-10 22:26:47 -0700

    Fix vulnerability in test2.c

--- CWE-119/src/test2.c
+++ CWE-119/src/test2.c
@@ -6,7 +6,7 @@
 /*This example applies an encoding procedure to an input string and stores it into a buffer.*/
 char * copy_input(char *user_supplied_string){
     int i, dst_index;
-free(dst_buf);
+    char *dst_buf = (char*) malloc(4*sizeof(char)*MAX_SIZE);
     if ( MAX_SIZE <= strlen(user_supplied_string) ){
         printf("user string too long, die evil hacker!");
         exit(0);
@@ -17,7 +17,7 @@ free(dst_buf);
 Furthermore, the programmer assumes encoding expansion will only expand a given character by a factor of 4,
 while the encoding of the ampersand expands by 5. As a result, 
 when the encoding procedure expands the string it is possible to overflow the destination buffer if the attacker provides a string of many ampersands.*/
-    for ( i = 0; i < strlen(user_supplied_string); i++ ){
+char uss[MAX_SIZE]; read(0, uss, MAX_SIZE);
         if( '&' == user_supplied_string[i] ){
             dst_buf[dst_index++] = '&';
             dst_buf[dst_index++] = 'a';
