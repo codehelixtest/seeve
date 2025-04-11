a5bb71d1ba231748239a879df1c69adad61d0eec
codehelixtest <rogern@nealit.com>
2025-04-10 22:27:16 -0700

    Fix vulnerability in test1.c

--- CWE-416/src/test1.c
+++ CWE-416/src/test1.c
@@ -14,8 +14,8 @@
         buf2R1 = (char *) malloc(BUFSIZER1);
         free(buf2R1);
         buf2R2 = (char *) malloc(BUFSIZER2);
-strncpy(buf2R1, argv[1], BUFSIZER1-1); // Move this line before free(buf2R1);
-        strncpy(buf2R1, argv[1], BUFSIZER1-1);
+        buf3R2 = (char *) malloc(BUFSIZER2);
+strncpy(buf2R1, argv[1], BUFSIZER1-1); // This line should be removed or corrected to use a valid buffer.
         free(buf1R1);
         free(buf2R2);
         free(buf3R2);
